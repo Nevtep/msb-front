6f9ba2ee44e0141af88106cd71e33ef60f668c82
Tomas Corellano <core@MacBook-Pro-de-Tomas.local>
2020-04-27 18:19:30 -0300

    Agrego link a telegram

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = React.forwardRef((props, ref) => (
             <section id="footer" ref={ref}>
                 <ul className="icons">
                     <li><a href="mailto:maximasebinarias@gmail.com" className="icon alt fa-2x fa-envelope"><span className="label">Gmail</span></a></li>
-                    <li><a href="#" className="icon alt fa-2x fa-telegram"><span className="label">Telegram</span></a></li>
+                    <li><a href="https://t.me/ALEXIS_CEO_MSB" className="icon alt fa-2x fa-telegram"><span className="label">Telegram</span></a></li>
                     <li><a href="#" className="icon alt fa-2x fa-youtube-play"><span className="label">Youtube</span></a></li>
                     <li><a href="https://www.instagram.com/maximasenalesbinarias" className="icon alt fa-2x fa-instagram"><span className="label">Instagram</span></a></li>
                     <li><a href="https://www.facebook.com/Maxima-Se%C3%B1ales-Binarias-100909861502722/" className="icon alt fa-2x fa-facebook"><span className="label">Facebook</span></a></li>
